ace2d754d4439615256e061a3884b22f1d23ca7a
jaclynpasquale <jaclynpasquale@gmail.com>
2015-01-09 00:02:49 -0600

    adding show edit view

---
+++ js/auth/authRoutes.js
@@ -0,0 +1,35 @@
+;(function(){
+  angular.module('fotdApp')
+  .config(function($routeProvider){
+    $routeProvider
+
+
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController',
+      controllerAs: 'loginCtrl',
+      resolve: {
+        data: function(authFactory){
+          authFactory.disallowLogin();
+        }
+      }
+    })
+    .when('/logout', {
+      template: '',
+      controller: 'LogoutController'
+    })
+    .when('/changepassword', {
+      templateUrl: 'views/changepassword.html',
+      controller: 'ChangePasswordController',
+      controllerAs: 'changepw',
+      private: true
+    })
+  })
+  .run(function($rootScope, authFactory){
+    $rootScope.$on('$routeChangeStart', function(event, nextRoute, priorRoute){
+      if (nextRoute.$$route && nextRoute.$$route.private) {
+        authFactory.requireLogin();
+      }
+    })
+  })
+}());
